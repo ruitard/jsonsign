d340a06b4c45272729b0ad20dd19015ab2959390
Ruitard <gallifrey.zhang@foxmail.com>
2021-12-16 01:22:07 +0800

    rename: licenseman => jsign

--- src/crypto/pk.cpp
+++ src/crypto/pk.cpp
@@ -14,7 +14,6 @@ private:
     mbedtls_pk_context       pk;
     mbedtls_entropy_context  entropy;
     mbedtls_ctr_drbg_context ctr_drbg;
-    buffer                   pers;
 
 public:
     Signer() noexcept {
@@ -22,28 +21,16 @@ public:
         mbedtls_entropy_init(&entropy);
         mbedtls_ctr_drbg_init(&ctr_drbg);
     }
-    explicit Signer(const buffer &pers) noexcept : Signer() { this->pers = pers; }
     Signer(const Signer &) = delete;
     Signer(Signer &&) = delete;
     auto operator=(const Signer &) -> Signer & = delete;
     auto operator=(Signer &&) -> Signer & = delete;
 
-    void load_key(const buffer &private_key, const buffer &password = buffer()) {
+    void load_key(const fs::path &private_keyfile) {
         int err = 0;
-        err = mbedtls_ctr_drbg_seed(&ctr_drbg, mbedtls_entropy_func, &entropy, pers.data(), pers.size());
+        err = mbedtls_ctr_drbg_seed(&ctr_drbg, mbedtls_entropy_func, &entropy, nullptr, 0);
         handle_mbedtls_error(err);
-
-        err = mbedtls_pk_parse_key(&pk, private_key.data(), private_key.size(), password.data(), password.size());
-        handle_mbedtls_error(err);
-    }
-
-    void load_key(const fs::path &private_keyfile, const std::string &password = std::string()) {
-        int err = 0;
-        // TODO(zuksan): 重复代码合并
-        err = mbedtls_ctr_drbg_seed(&ctr_drbg, mbedtls_entropy_func, &entropy, pers.data(), pers.size());
-        handle_mbedtls_error(err);
-
-        err = mbedtls_pk_parse_keyfile(&pk, private_keyfile.c_str(), password.c_str());
+        err = mbedtls_pk_parse_keyfile(&pk, private_keyfile.c_str(), nullptr);
         handle_mbedtls_error(err);
     }
 
@@ -82,11 +69,6 @@ public:
     auto operator=(const Verifier &) -> Verifier & = delete;
     auto operator=(Verifier &&) -> Verifier & = delete;
 
-    void load_key(const buffer &public_key) {
-        int err = mbedtls_pk_parse_public_key(&pk, public_key.data(), public_key.size());
-        handle_mbedtls_error(err);
-    }
-
     void load_key(const fs::path &public_keyfile) {
         int err = mbedtls_pk_parse_public_keyfile(&pk, public_keyfile.c_str());
         handle_mbedtls_error(err);
@@ -104,22 +86,10 @@ public:
     ~Verifier() noexcept { mbedtls_pk_free(&pk); }
 };
 
-auto sign(const buffer &content, const buffer &private_key, const buffer &password) -> buffer {
+auto sign(const buffer &content, const fs::path &private_keyfile) -> buffer {
     Signer signer;
-    signer.load_key(private_key, password);
+    signer.load_key(private_keyfile);
     return signer.sign(content);
-}
-
-auto sign(const buffer &content, const fs::path &private_keyfile, const std::string &password) -> buffer {
-    Signer signer;
-    signer.load_key(private_keyfile, password);
-    return signer.sign(content);
-}
-
-auto verify(const buffer &content, const buffer &signature, const buffer &public_key) -> bool {
-    Verifier verifier;
-    verifier.load_key(public_key);
-    return verifier.verify(content, signature);
 }
 
 auto verify(const buffer &content, const buffer &signature, const fs::path &public_keyfile) -> bool {
--- src/crypto/pk.hpp
+++ src/crypto/pk.hpp
@@ -6,10 +6,8 @@ namespace licenseman {
 
 namespace pk {
 
-buffer sign(const buffer &content, const buffer &private_key, const buffer &password = buffer());
-buffer sign(const buffer &content, const fs::path &private_keyfile, const std::string &password = std::string());
+buffer sign(const buffer &content, const fs::path &keyfile);
 
-bool verify(const buffer &content, const buffer &signature, const buffer &public_key);
 bool verify(const buffer &content, const buffer &signature, const fs::path &public_keyfile);
 
 namespace rsa {
